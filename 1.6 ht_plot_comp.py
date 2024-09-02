70834addca78ef0f0e0b43e6e7256f007d2a721f
mzamzam <mzn5412@gmail.com>
2024-09-02 09:42:46 +0700

    Plot for different slicing

--- 1.6 ht_plot_comp.py
+++ 1.6 ht_plot_comp.py
@@ -48,7 +48,8 @@ dt_intensity['minute'] = dt_intensity['date'].apply(lambda i:i - dt_intensity['d
 dt_intensity['minute'] = dt_intensity['minute'].apply(lambda i:i.seconds/60)
 start_time = dt_intensity.loc[1].date
 end_time = dt_intensity.loc[dt_intensity.shape[0]-1].date
-edge_coord_mid = pd.read_csv(f"Results/canny/edge_coord_{dtdt}.csv", index_col='Unnamed: 0')
+sig = 8
+edge_coord_mid = pd.read_csv(f"Results/canny/edge_coord_{dtdt}_{sig}.csv", index_col='Unnamed: 0')
 
 def model_ht(angular_separation, edge_coordinate):
     ang_sep = angular_separation
@@ -57,30 +58,87 @@ def model_ht(angular_separation, edge_coordinate):
     edge_coord['x_second'] = edge_coord.x_minute*60
     edge_coord['y_Mm'] = edge_coord['y'].apply(lambda i:ang_sep.loc[i].h_km)/1000
     edge_coord.drop_duplicates(subset=['x'], inplace=True)
+    for i in range(edge_coord.shape[0] - 20, edge_coord.shape[0], 1):
+        start_slice = 0
+        end_slice = i  ## or can be used: edge_coord.shape[0]
+        t_minute = edge_coord.x_minute[start_slice:end_slice]
+        h_Mm = edge_coord.y_Mm[start_slice:end_slice]   ## different data slicing resulting in different result
+        h_arcsec = edge_coord.y_arcsec[start_slice:end_slice]
+        c0 = h_Mm.iloc[0]
+        t0 = t_minute.min()
+        tau = (t_minute.max()) - (t_minute.min()) / 10
+        c1 = 0.1  # (h_Mm.iloc[-1] - h_Mm.iloc[0]) / (t_minute.iloc[-1] - t_minute.iloc[0])
+        c2 = 0
+        try:
+            popt, pcov = curve_fit(eq_h, t_minute, h_Mm, p0=[c0, tau, c1, c2, t0])
+            c0_opt, tau_opt, c1_opt, c2_opt, t0_opt = popt
+        except RuntimeError:
+            continue
+        t_minute_model = np.linspace(min(t_minute), max(t_minute), int(max(t_minute)-min(t_minute)))
+        h_Mm_model = eq_h(t_minute_model,c0_opt,tau_opt,c1_opt,c2_opt, t0_opt)
+        dft_model = pd.Series(t_minute_model)
+        t_minute_model_dt = dft_model.apply(lambda i:start_time + timedelta(minutes=i))
+        t_minute_dt = t_minute.apply(lambda i:start_time + timedelta(minutes=i))
 
-    start_slice = 0
-    end_slice = 70   ## or can be used: edge_coord.shape[0]
-    t_minute = edge_coord.x_minute[start_slice:end_slice]
-    h_Mm = edge_coord.y_Mm[start_slice:end_slice]   ## different data slicing resulting in different result
-    h_arcsec = edge_coord.y_arcsec[start_slice:end_slice]
+        t_minute_onset = t_ons(tau_opt, c1_opt, c0_opt, t0_opt)
+        if t_minute_onset != np.nan:
+            try:
+                t_minute_onset_model = t_minute_model[t_minute_model >= t_minute_onset][0]
+                t_onset_datetime = start_time + timedelta(minutes=t_minute_onset_model)
+                h_onset_Mm_data = edge_coord[edge_coord.x_minute >= t_minute_onset_model].y_Mm.values[0]
+                h_onset_arcsec_datax = edge_coord[edge_coord.x_minute >= t_minute_onset_model].y_arcsec.values[0]
+                h_onset_Mm_model = h_Mm_model[np.argwhere(t_minute_model == t_minute_onset_model)[0][0]]
+                h_onset_arcsec_datay = edge_coord[edge_coord.y_Mm >= h_onset_Mm_model].y_arcsec.values[0]
+                fig, ax = plt.subplots()
+                plt.rc('xtick', labelsize=12)
+                plt.rc('ytick', labelsize=12)
+                plt.rc('axes', labelsize=12)
 
-    popt, pcov = curve_fit(eq_h, t_minute, h_Mm, p0=[7, 30, 0.1, 89, 211])   ## guessing p0 is tricky, need alot of trials and errors.
-    c0_opt, tau_opt,c1_opt,c2_opt,t0_opt = popt
-    t_minute_model = np.linspace(min(t_minute), max(t_minute), int(max(t_minute)-min(t_minute)))
-    h_Mm_model = eq_h(t_minute_model,c0_opt,tau_opt,c1_opt,c2_opt, t0_opt)
-    dft_model = pd.Series(t_minute_model)
-    t_minute_model_dt = dft_model.apply(lambda i:start_time + timedelta(minutes=i))
-    t_minute_dt = t_minute.apply(lambda i:start_time + timedelta(minutes=i))
+                orig_map = matplotlib.colormaps.get_cmap('sdoaia304')
+                reversed_map = orig_map.reversed()
+                xlim = [dt_intensity.date[0], dt_intensity.date[len(dt_intensity) - 1]]
+                x_lim = mdates.date2num(xlim)
+                ax.imshow(image_binned, cmap=reversed_map, origin='lower', vmax=35,
+                          extent=[x_lim[0], x_lim[1], 0, ang_sep_mid.h_arcsec.max()], aspect='auto')
+                one_km_to_arcsec = 1 / one_arcsec_to_km
+                h_arcsec_model = (
+                                             h_Mm_model * 1000) * one_km_to_arcsec  ## only have fitting line in Mm, need to convert the to arcsec
+                ax.plot(t_minute_model_dt, h_arcsec_model, color='k')  ## plot fitting line but in arcsec
+                ax.set_ylabel('Distance along slice (arcsec)', fontsize=18)
+                ax.set_xlabel('Start time = {}'.format(start_time.strftime('%Y/%m/%d %H:%M:%S')), fontsize=18)
+                #ax.set_ylim(0, 470)
+                ax.text(dt_intensity.date[10], 420, '(b)', fontsize=20)
+                ax.text(t_onset_datetime - timedelta(minutes=45), h_onset_arcsec_datay + 13,
+                        f'{round(h_onset_Mm_model)} Mm', fontsize=13)
+                ax.plot(t_onset_datetime, h_onset_arcsec_datay, 'k*', ms=10)
 
-    t_minute_onset = t_ons(tau_opt, c1_opt, c0_opt, t0_opt)
-    t_minute_onset_model = t_minute_model[t_minute_model >= t_minute_onset][0]
-    t_onset_datetime = start_time + timedelta(minutes=t_minute_onset_model)
-    h_onset_Mm_data = edge_coord[edge_coord.x_minute >= t_minute_onset_model].y_Mm.values[0]
-    h_onset_arcsec_datax = edge_coord[edge_coord.x_minute >= t_minute_onset_model].y_arcsec.values[0]
-    h_onset_Mm_model = h_Mm_model[np.argwhere(t_minute_model == t_minute_onset_model)[0][0]]
-    h_onset_arcsec_datay = edge_coord[edge_coord.y_Mm >= h_onset_Mm_model].y_arcsec.values[0]
+                ax.yaxis.set_minor_locator(ticker.MultipleLocator(10))
+
+                ax.xaxis.set_minor_locator(mdates.MinuteLocator(interval=20))
+                # ax.xaxis_date()
+                date_format = mdates.DateFormatter('%H:%M')
+                ax.xaxis.set_major_formatter(date_format)
+                ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=90))
+                ax.xaxis.set_tick_params(labelsize=16)
+                ax.yaxis.set_tick_params(labelsize=16)
+
+                ax2 = ax.secondary_xaxis('top')
+                ax2.xaxis.set_tick_params(labeltop=False)
+                ax2.xaxis.set_minor_locator(mdates.MinuteLocator(interval=20))
+                ax2.xaxis.set_major_locator(mdates.MinuteLocator(interval=80))
+
+                ax3 = ax.secondary_yaxis('right')
+                ax3.yaxis.set_tick_params(labelright=False)
+                ax3.yaxis.set_minor_locator(ticker.MultipleLocator(10))
+                os.makedirs('Results/ht_plot_comp', exist_ok=True)
+                plt.savefig(f'Results/ht_plot_comp/{sig}_fig_6b_{dtdt}_{i}.png', bbox_inches='tight', dpi=100)
+                #plt.show()
+                plt.close()
+            except IndexError:
+                continue
     return c0_opt, tau_opt, c1_opt, c2_opt, t0_opt, t_minute_onset, t_onset_datetime, h_onset_Mm_model, h_onset_arcsec_datay, t_minute_model_dt, h_Mm_model, start_time, t_minute, t_minute_dt, h_Mm, h_arcsec
 c0_opt, tau_opt, c1_opt, c2_opt, t0_opt, t_minute_onset, t_onset_datetime, h_onset_Mm_model, h_onset_arcsec_datay, t_minute_model_dt, h_Mm_model, start_time, t_minute, t_minute_dt, h_Mm, h_arcsec = model_ht(ang_sep_mid, edge_coord_mid)
+'''
 print(f'optimum parameter (c0, tau, c1, c2, t0): {round(c0_opt,3), round(tau_opt,3), round(c1_opt,3), round(c2_opt,3), round(t0_opt,3)}')
 print('onset time: {} UT'.format(t_onset_datetime.strftime('%Y/%m/%d %H:%M')))
 print(f'onset height: {h_onset_Mm_model} Mm')
@@ -124,6 +182,6 @@ ax3 = ax.secondary_yaxis('right')
 ax3.yaxis.set_tick_params(labelright=False)
 ax3.yaxis.set_minor_locator(ticker.MultipleLocator(10))
 os.makedirs('Results/ht_plot_comp', exist_ok=True)
-plt.savefig(f'Results/ht_plot_comp/fig_6b_{dtdt}.png',bbox_inches='tight', dpi=100)
+plt.savefig(f'Results/ht_plot_comp/fig_6b_{dtdt}_{}.png',bbox_inches='tight', dpi=100)
 plt.show()
-
+'''
